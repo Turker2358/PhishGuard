f1ae2f7682d2952c5a559d700c26c5cfdc8737a8
ktt <149545592+ktt51@users.noreply.github.com>
2026-09-09 23:15:13 +0800

    Update __init__.py

--- src/phishing_detector/detectors/__init__.py
+++ src/phishing_detector/detectors/__init__.py
@@ -3,5 +3,6 @@
 from phishing_detector.detectors.base import Detector
 from phishing_detector.detectors.content import ContentLLMDetector
 from phishing_detector.detectors.pattern import PatternDetector
+from phishing_detector.detectors.attachment import AttachmentDetector
 
-__all__ = ["ContentLLMDetector", "Detector", "PatternDetector"]
+__all__ = ["ContentLLMDetector", "Detector", "PatternDetector","AttachmentDetector"]
